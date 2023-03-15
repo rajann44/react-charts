9ef4b78826bfe76ac87a7fb85464d364f63701e9
Rajan Chaudhary <rajann44@gmail.com>
2023-03-16 00:12:59 +0100

    kuch work

--- src/components/funtionality/Chart.js
+++ src/components/funtionality/Chart.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
     BarChart,
     Bar,
@@ -9,10 +10,66 @@ import {
     ResponsiveContainer,
   } from "recharts";
 
-export default function Chart({ data }){
+var json = require('../../utils/result.json'); //(with path)
+
+
+
+export default function Chart(){
+
+useEffect(()=>{
+  console.log(json)
+  console.log(json.name)
+  json.map(function(element){
+    console.log(element.name);
+  })
+},[]);
+
+  const data = [
+    {
+      id: 1,
+      name: "Anastassia",
+      email: "anolot0@topsy.com",
+      gender: "Female",
+      age: 42,
+      phone: 9569440410
+    },
+    {
+      id: 2,
+      name: "Matty",
+      email: "mgoldes1@reuters.com",
+      gender: "Female",
+      age: 98,
+      phone: 9628922550
+    },
+    {
+      id: 3,
+      name: "Antonio",
+      email: "afawlo2@geocities.jp",
+      gender: "Male",
+      age: 54,
+      phone: 9609654493
+    },
+    {
+      id: 4,
+      name: "Neddie",
+      email: "ncumine3@smugmug.com",
+      gender: "Male",
+      age: 31,
+      phone: 9424429154
+    },
+    {
+      id: 5,
+      name: "Danit",
+      email: "dolifaunt4@wikispaces.com",
+      gender: "Female",
+      age: 35,
+      phone: 9428847328
+    },
+  ];
 
     return(<>
     <div className='my-10'>
+      
     <ResponsiveContainer width="100%" height={400}>
       <BarChart
         data={data}
@@ -25,11 +82,10 @@ export default function Chart({ data }){
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
-        <YAxis />
+        <YAxis dataKey="age"/>
         <Tooltip />
         <Legend />
-        <Bar dataKey="name" fill="#8884d8" />
-        <Bar dataKey="priceUsd" fill="#82ca9d" />
+        <Bar dataKey="age" fill="#82ca9d" />
       </BarChart>
     </ResponsiveContainer>
     </div>
