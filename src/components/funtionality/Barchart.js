dd78c7c8463453851b1abe87482d3a1e10e0a090
Rajan Chaudhary <rajann44@gmail.com>
2023-03-17 20:58:43 +0100

    ui enhancement

--- src/components/funtionality/Barchart.js
+++ src/components/funtionality/Barchart.js
@@ -10,16 +10,14 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const json = require('../../utils/result.json'); //(with path)
+const json = require("../../utils/result.json"); //(with path)
 
 var columnData2 = null;
 let parsedData;
 
 export default function Barchart() {
-
   const [text, setText] = useState("");
   const [columnData, setcolumnData] = useState(null);
-  
 
   const handleOnChange = (event) => {
     setText(event.target.value);
@@ -31,7 +29,7 @@ export default function Barchart() {
     parsedData = await sheetResponse.json();
     columnData2 = Object.keys(parsedData[0]);
     setcolumnData(columnData2);
-    console.log(columnData)
+    console.log(columnData);
   }
 
   function handleOnClick() {
@@ -39,88 +37,101 @@ export default function Barchart() {
   }
 
   useEffect(() => {
-    console.log("EFFECT:"+columnData);
+    console.log("EFFECT:" + columnData);
   }, [columnData]);
 
-  function onXClickHandler(c){
+  function onXClickHandler(c) {
     setXAxisValue(c);
   }
-  function onYClickHandler(c){
+  function onYClickHandler(c) {
     setYAxisValue(c);
   }
 
-  const[xAxisValue, setXAxisValue] = useState();
-  const[yAxisValue, setYAxisValue] = useState();
+  const [xAxisValue, setXAxisValue] = useState();
+  const [yAxisValue, setYAxisValue] = useState();
 
-  return (<>
-  <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter Location"
-              aria-label="Location"
-              aria-describedby="button-addon2"
-              value={text}
-              onChange={handleOnChange}
-            />
-            <button
-              className="btn btn-outline-secondary"
-              onClick={() => handleOnClick()}
-              type="button"
-              id="button-addon2"
-            >
-              Fetch Sheet Data!!
-            </button>
-          </div>
-  <div className="d-flex flex-row">
-  {columnData!=null && <div className="dropdown p-2">
-      <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        X-Axis
-      </button>
-      <ul className="dropdown-menu">
-            {columnData.map((c)=>{
-                    return <li key={c}><a 
-                    onClick={() => onXClickHandler(c)}
-                    className="dropdown-item">
-                    {c}
-                    </a></li>
-            })}
-      </ul>
-    </div>}
-    {columnData!=null && <div className="dropdown p-2">
-      <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Y-Axis
-      </button>
-      {columnData!=null && <ul className="dropdown-menu">
-            {columnData.map((c)=>{
-                    return <li key={c}><a 
-                    onClick={() => onYClickHandler(c)}
-                    className="dropdown-item">
-                    {c}
-                    </a></li>
-            })}
-      </ul>}
-    </div>}
-    </div>
-    <div className='my-10'>
-      <ResponsiveContainer width="100%" height={400}>
-        <BarChart
-          data={parsedData}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
+  return (
+    <div className="container">
+      <div className="input-group w-50 my-3 mx-auto">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter Gsheet ID"
+          onChange={handleOnChange}
+          value={text}
+        />
+        <button
+          className="btn btn-success"
+          type="button"
+          id="button-addon2"
+          onClick={() => handleOnClick()}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey={xAxisValue} />
-          <YAxis dataKey={yAxisValue} />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey={yAxisValue} fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
+          Fetch Now
+        </button>
+      </div>
+      
+      <div className="my-10">
+        <ResponsiveContainer width="100%" height={400}>
+          <BarChart
+            data={parsedData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey={xAxisValue} />
+            <YAxis dataKey={yAxisValue} />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey={yAxisValue} fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+      
+
+      <div className="d-flex flex-row justify-content-center">
+        {columnData != null && (
+          <div className="dropdown p-2">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              X-Axis
+            </button>
+            <ul className="dropdown-menu">
+              {columnData.map((c) => {
+                return (
+                  <li key={c}>
+                    <a onClick={() => onXClickHandler(c)} className="dropdown-item">
+                      {c}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+        {columnData != null && (
+          <div className="dropdown p-2">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Y-Axis
+            </button>
+            {columnData != null && (
+              <ul className="dropdown-menu">
+                {columnData.map((c) => {
+                  return (
+                    <li key={c}>
+                      <a onClick={() => onYClickHandler(c)} className="dropdown-item">
+                        {c}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+        )}
+      </div>
     </div>
-  </>);
+  );
 }
--- src/components/reusable/Navbar.js
+++ src/components/reusable/Navbar.js
@@ -1,28 +1,64 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">
-      <img src="https://okrj.files.wordpress.com/2021/07/ezgif-7-7a1a9fb118d3.gif" alt="Bootstrap" width="44" height="44"/>
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
-        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/chart">Chart</Link></li>
-        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/line">Line</Link></li>
-        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/file">File</Link></li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img
+            src="https://okrj.files.wordpress.com/2021/07/ezgif-7-7a1a9fb118d3.gif"
+            alt="Bootstrap"
+            width="44"
+            height="44"
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/chart">
+                Chart
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/line">
+                Line
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/file">
+                File
+              </Link>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
